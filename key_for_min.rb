6857dcdc2f0a0e56286de39eaaec81fdd23dd7fe
Tim Rinkerman <tim.rinkerman98@gmail.com>
2020-06-15 02:27:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
+#pre: a hash is presented multiple keys and values
+#post: the hash has been iterated over, each value has been compared to one another and the key of the lowest value is returned 
 def key_for_min_value(name_hash)
-
+  f
 end
